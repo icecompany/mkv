a483bb15bca0b2c37eb5fee515d7d440a4e8c771
xakepok@xakepok.com <44duP_yx579>
2020-06-22 18:39:35 +0300

    Only managers in filter by manager

--- administrator/components/com_mkv/models/fields/manager.php
+++ administrator/components/com_mkv/models/fields/manager.php
@@ -13,9 +13,20 @@ class JFormFieldManager extends JFormFieldList
         $db = JFactory::getDbo();
         $query = $db->getQuery(true);
         $query
-            ->select("u.id, u.name")
+            ->select("distinct u.id, u.name")
             ->from("`#__users` u")
             ->order("u.name");
+        $projectID = PrjHelper::getActiveProject();
+        if ($projectID > 0) {
+            $groupID = $this->getProjectGroup($projectID);
+            $query
+                ->leftJoin("#__user_usergroup_map m on m.user_id = u.id")
+                ->where("m.group_id = {$db->q($groupID)}");
+        }
+        $userID = JFactory::getUser();
+        if ($userID !== 377) {
+            $query->where("u.id not in (377, 440, 441, 442, 447, 427)");
+        }
         $result = $db->setQuery($query)->loadObjectList();
 
         $options = array();
@@ -29,6 +40,17 @@ class JFormFieldManager extends JFormFieldList
         }
 
         return $options;
+    }
+
+    private function getProjectGroup(int $projectID): int
+    {
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true);
+        $query
+            ->select("groupID")
+            ->from("#__mkv_projects")
+            ->where("id = {$db->q($projectID)}");
+        return $db->setQuery($query)->loadResult() ?? 0;
     }
 
     public function getOptionsExternally()
